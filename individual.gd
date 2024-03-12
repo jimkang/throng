d8ef30cb66da45f42ab68d37edc9ba161df5ec85
Jim Kang <jimkang@gmail.com>
2024-03-11 21:48:34 -0400

    Place individuals within tiles. Scale up.

--- individual.gd
+++ individual.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
+@export var color_set: PackedColorArray 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Sprite2D.modulate = color_set[randi_range(0, color_set.size()-1)]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- throng_root_node.gd
+++ throng_root_node.gd
@@ -11,6 +11,8 @@ var Alligator = preload("res://alligator.tscn")
 var tile_indexes_for_names = {
 	'parquet': Vector2i(16, 0)
 }
+const half_unit_vec = Vector2(0.5, 0.5)
+
 #var dungeon_tile_set = preload('res://throng_dungeon_tile_set.tres').instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,18 +20,26 @@ func _ready():
 	tilemap.add_layer(0)
 	var floor_points = $map_gen.generate_map(
 		randi_range(map_gen_iteration_range[0], map_gen_iteration_range[0]),
-		map_gen_branch_len_range, map_dimensions
+		map_gen_branch_len_range,
+		map_dimensions
 	)
 
 	for point in floor_points:
 		tilemap.set_cell(0, point, 0, tile_indexes_for_names.parquet) 
 	
+	var possible_individual_locations = floor_points.duplicate()
 	var player = Individual.instantiate()
-	player.position = floor_points.pick_random() * tile_size
+	var player_location = Vector2(possible_individual_locations.pick_random())
+	# position is the origin of the node. Since the node centers its children
+	# around the origin, we have to put the position in the center of the tile.
+	player.position = (player_location + half_unit_vec) * tile_size
+	possible_individual_locations.erase(player_location)
 	add_child(player)
 	
 	var alligator = Alligator.instantiate()
-	alligator.position = floor_points.pick_random() * tile_size
+	var alligator_location = Vector2(possible_individual_locations.pick_random())
+	alligator.position = (alligator_location + half_unit_vec) * tile_size
+	possible_individual_locations.erase(alligator_location)
 	add_child(alligator)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
