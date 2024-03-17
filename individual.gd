d3f8bd7e6732fc58fe847eaf3af13e1938966d74
Jim Kang <jimkang@gmail.com>
2024-03-17 12:05:22 -0400

    Cache tilemap ref. Drop unnecessary var.

--- individual.gd
+++ individual.gd
@@ -2,10 +2,12 @@ class_name Individual
 extends Node2D
 
 @export var color_set: PackedColorArray
+var tilemap: TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite2D.modulate = color_set[randi_range(0, color_set.size()-1)]
+	tilemap = self.get_node('/root/throng_root_node/dungeon_tilemap')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,18 +16,15 @@ func _process(delta):
 # Returns true if we were actually able to move.
 func move(move_vector: Vector2, result_array: Array):
 	var next_pos = self.position + move_vector
-	var tilemap: TileMap = self.get_node('/root/throng_root_node/dungeon_tilemap')
 	var next_cell_pos = tilemap.local_to_map(tilemap.to_local(next_pos))
 	var dest_cell_data = tilemap.get_cell_tile_data(0, next_cell_pos)
 	var result = false
 	if dest_cell_data:
-		var is_floor = dest_cell_data.get_custom_data('is_floor')		
-		if is_floor:
+		if dest_cell_data.get_custom_data('is_floor'):
 			var colliding_thing = Collision.find_colliding_things_at(
 				get_world_2d().direct_space_state, next_pos)
 			if colliding_thing:
 				if colliding_thing.get_meta('individual'):
-					# TODO: Make collision check a static function.
 					act_on_other(colliding_thing)
 			else:
 				self.position += move_vector
