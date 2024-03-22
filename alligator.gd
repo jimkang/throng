535e963b763ab5c09fb0f5ea3c6ddf0413b49c85
Jim Kang <jimkang@gmail.com>
2024-03-22 18:59:57 -0400

    Make the alligator sometimes stay still.

--- alligator.gd
+++ alligator.gd
@@ -1,9 +1,14 @@
 class_name Alligator extends Individual
 
+var cardinal_directions = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+
 func take_turn(event):
 	super.take_turn(event)
+	if (randi_range(0, 3) > 2):
+		return
+
 	var tile_size = $/root/throng_root_node.tile_size
-	self.move([Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN].pick_random() * tile_size)
+	self.move(self.cardinal_directions.pick_random() * tile_size)
 
 func act_on_other(other: Individual):
 	var turn_over = bite(other)
