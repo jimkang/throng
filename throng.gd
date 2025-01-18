8928b6364a3b70a726699be597045ef58cba6383
Jim Kang <jimkang@gmail.com>
2025-01-17 20:23:21 -0500

    Make throng respond to input again.

--- throng.gd
+++ throng.gd
@@ -11,7 +11,7 @@ func _ready():
 	self.add_to_group('throngs')
 	#self.add_to_group('actors')
 
-func take_turn(event: InputEvent):
+func _unhandled_key_input(event: InputEvent) -> void:
 	var x = 0
 	var y = 0
 	if event.is_action_released('ui_left'):
